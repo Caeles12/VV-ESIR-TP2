de13f670f6798880b3ed00ed8922d5f5c7591833
fannyshehabi <fanny.shehabi@etudiant.univ-rennes.fr>
2023-12-05 17:35:31 +0100

    Starter Ex5

---
+++ code/Exercise5/javaparser-starter/src/main/java/fr/istic/vv/CyclomaticComplexity.java
@@ -0,0 +1,9 @@
+package fr.istic.vv;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
+
+public class CyclomaticComplexity extends VoidVisitorWithDefaults<Void> {
+    
+}
